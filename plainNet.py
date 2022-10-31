e429112418b12a6fceed333f3d86e0753d4cd1b1
Emil Johannesen Haugstvedt <59867417+emiljha@users.noreply.github.com>
2022-10-31 19:48:27 +0100

    plainNet implemented

--- plainNet.py
+++ plainNet.py
@@ -12,9 +12,9 @@ class plainNet(torch.nn.Module):
 
         self.input = nn.Linear(n_inputs, hidden_sizes[0][0])
 
-        self.layers = []
-        for size in hidden_sizes:
-            self.layers.append(nn.Linear(size[0], size[1]))
+        self.layers = nn.ModuleList()
+        for layer in hidden_sizes:
+            self.layers.append(nn.Linear(layer[0], layer[1]))
         
         self.output = nn.Linear(hidden_sizes[-1][-1], n_outputs)
 
@@ -25,10 +25,22 @@ class plainNet(torch.nn.Module):
         for layer in self.layers:
             x = F.relu(layer(x))
     
-        x = F.relu(self.output(x))
+        x = self.output(x)
 
         return x
 
-    def train_n_epochs(n_epochs):
+    def train_n_epochs(self, training_loader, n_epochs, lr = 0.001, verbose=False):
+        optimizer =  torch.optim.Adam(self.parameters(), lr=0.001)
+        loss_fn = torch.nn.MSELoss()
 
-        
+        for epoch in range(n_epochs):
+            for (x_batch, y_batch) in training_loader:
+                optimizer.zero_grad()
+                y_pred = self.forward(x_batch)
+                loss = loss_fn(y_pred, y_batch)
+                loss.backward()
+                optimizer.step()
+
+            if (epoch % 100 == 0) and verbose:
+                print('Epoch {}: loss {}'.format(epoch, loss.item()))
+
