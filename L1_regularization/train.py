fd74feb810a46c04403d6a990301437c63f30cd8
Emil Johannesen Haugstvedt <59867417+emiljha@users.noreply.github.com>
2022-11-28 17:30:13 +0100

    Improve training

--- L1_regularization/train.py
+++ L1_regularization/train.py
@@ -2,6 +2,8 @@ import torch
 from torch.utils.data import dataloader
 import sys
 import pickle
+import os
+from tqdm import tqdm
 
 sys.path.append("../")
 
@@ -38,17 +40,38 @@ n_models = 10
 input_size = train_loader.dataset.x.shape[1]
 output_size = train_loader.dataset.y.shape[1]
 
-layers = [input_size, 64, 64, 64, output_size]
+layers = [input_size, 64, 64, 64, 64, output_size]
 
 for n in range(n_models):
 
+    print(f"### Training model {n+1} ###")
+
+    run = neptune.init_run(
+        project="emilhaugstvedt/l1-regularization",
+        name=f"model_{n+1}",
+        api_token=os.environ["NEPTUNE"]
+    )
+
+    run["hyperparameters"] = {
+        "n_epochs": n_epochs,
+        "lr": lr,
+        "weight_decay": weight_decay,
+        "l1": l1,
+        "layers": layers
+    }
+
     model = L1RegularizationNet(layers=layers)
+
+    lowest_val_loss = 99999
+    best_model = model
 
     ### Train model ###            
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+    #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=0.9)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=10, gamma=0.9) # Endre med gamm 0.1 etter 50 epochs
     criterion = torch.nn.MSELoss()
 
-    for epoch in range(n_epochs):
+    for epoch in tqdm(range(n_epochs)):
         for (x_batch, y_batch) in train_loader:
             model.train()
             optimizer.zero_grad()
@@ -56,6 +79,7 @@ for n in range(n_models):
             loss = criterion(y_pred, y_batch) + l1 * model.L1_loss()
             loss.backward()
             optimizer.step()
+        scheduler.step()
 
         model.eval()
         with torch.no_grad():
@@ -66,10 +90,21 @@ for n in range(n_models):
 
         # Log train loss, validation loss and sparsity
 
-        if (epoch % 10 == 0):
-                print(f"Epoch {epoch}")
-                print(f"Train loss: {loss}")
-                print(f"Validation loss: {val_loss}")
-                print(f"Sparsity: {model.get_sparsity()} \n")
+        #if (epoch % 10 == 0):
+        #        print(f"Epoch {epoch}")
+        #        print(f"Train loss: {loss}")
+        #        print(f"Validation loss: {val_loss}")
+        #        print(f"Sparsity: {model.get_sparsity()} \n")
 
-    torch.save(model, f"../models/{dataset}/L1_regularization/model_{n+1}.pickle")
+        run["train/loss"].log(loss)
+        run["validation/loss"].log(val_loss)
+        run["sparsity"].log(model.get_sparsity())
+        run["lr"].log(optimizer.param_groups[0]["lr"])
+
+        if val_loss < lowest_val_loss:
+            lowest_val_loss = val_loss
+            best_model = model
+
+    run.stop()
+    #torch.save(model, f"../models/{dataset}/L1_regularization/model_{n+1}.pickle")
+    torch.save(model, f"../models/{dataset}/L1_regularization/best_model_{n+1}.pickle")
