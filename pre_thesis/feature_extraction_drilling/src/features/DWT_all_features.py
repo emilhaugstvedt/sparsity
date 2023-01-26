2e78c91dc8c63ac5ee7eb507bad4584b42e0c4eb
Emil Johannesen Haugstvedt <emiljha@stud.ntnu.no>
2023-01-26 18:47:07 +0100

    All feature extractions working and extracted

--- pre_thesis/feature_extraction_drilling/src/features/DWT_all_features.py
+++ pre_thesis/feature_extraction_drilling/src/features/DWT_all_features.py
@@ -53,13 +53,13 @@ def get_features(filename, data):
     return pd.Series(np.hstack((ft0_trunc , ft1_trunc, ft2_trunc, ft3_trunc, ft4_trunc, ft5_trunc)))
     
 
-def get_column_names(decomp_levels):
+def get_column_names(feature, decomp_levels):
     stats = ['mean' , 'std' , 'skew', 'max', 'median', 'min']
     stats_HOS = stats + ['energy', 'entropy']
     others = ['zcr', 'spec_roll_off' , 'spec_centroid', 'spec_contrast', 'spec_bandwidth']
     cols = []
     for n in range(decomp_levels):
-        base = f'level_{n}_'
+        base = f'{feature}_level_{n}_'
         for stat in stats_HOS:
             cols.append(base + stat)    
            
@@ -69,20 +69,8 @@ def get_column_names(decomp_levels):
     return cols
 
 
-def get_dataset(n_levels = 10):
+def get_dataset(data, n_levels = 10):
     random.seed(20)
-    with open('data/Case_2_a_only_basic_DQ', 'rb') as f:
-        ((data1_1_df, data1_2_df, data1_3_df),(mean1_df,std1_df)) = pickle.load(f)
-
-    data1_df = pd.concat([data1_1_df,data1_2_df,data1_3_df],axis=0)
-    data1_df = data1_df * std1_df + mean1_df
-
-    (imin,_) = next((i, el) for i, el in enumerate(data1_df.HDEP.values) if el < 200)
-    data = data1_df.iloc[imin:]
-
-    data = data["DHT001_ECD"].values
-
-    print("Data loaded")
 
     chopped_timeseries = chop_timeseries(data, 1000)
 
@@ -104,16 +92,31 @@ def get_dataset(n_levels = 10):
             row['name'] = [i]
             row = row['name'].apply(get_features, data = cD)
             features = pd.concat([features, row], ignore_index=True, axis=1)
-            
-        df = pd.concat([features, df], ignore_index=True)
-    df.columns = get_column_names(n_levels)
+        df = pd.concat([df, features], ignore_index=True)
+
     return df
 
 def main():
+    with open('data/Case_2_a_only_basic_DQ', 'rb') as f:
+        ((data1_1_df, data1_2_df, data1_3_df),(mean1_df,std1_df)) = pickle.load(f)
+
+    data1_df = pd.concat([data1_1_df,data1_2_df,data1_3_df],axis=0)
+    data1_df = data1_df * std1_df + mean1_df
+
+    (imin,_) = next((i, el) for i, el in enumerate(data1_df.HDEP.values) if el < 200)
+    data = data1_df.iloc[imin:]
+
+    features = ["ASMPAM1_T", "ASMPAM2_T", "ASMPAM3_T", "FLIAVG", "FLOAVG", "HKLDAV", "ROPA"]
+
+    data = data[features].iloc[1130000:1230000]
+    print("Data loaded")
+    print("Creating features.")
     start = time.time()
-    df =  get_dataset(n_levels = 10)
-    print(f'Shape of features: {df.shape}')
-    df.to_csv(f'features/new_features/DWT_complete_samples.csv')
+    for feature in features:
+        print(f"Creating feature for {feature}")
+        df =  get_dataset(data[feature].values, n_levels = 10)
+        df.columns = get_column_names(feature, decomp_levels=10)
+        df.to_csv(f'features/new_features/{feature}/DWT_complete_samples.csv')
     print(f' Processing finished, total time used = {time.time() - start}')
 
 main()
